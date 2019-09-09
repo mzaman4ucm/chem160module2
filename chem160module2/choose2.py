dc725bded9164be12eea1f19e0c1dfccaaea727b
mzaman4ucm <54760984+mzaman4ucm@users.noreply.github.com>
2019-09-08 23:15:47 -0700

    Delete choose2.py

--- chem160module2/choose2.py
+++
@@ -1,9 +0,0 @@
-from random import choices
-ntrials = 100000
-limit = 100
-count = 0
-for i in range(ntrials):
-    rand = choices(range(1, limit+1), k=3)
-    if rand[0] + rand[1] <= rand[2]:
-        count = count+1
-print("Fraction =", count/ntrials)
